a09660852e469e204fad6e3dc0b1f6d23d63308d
OzerOzturk <ozerozturk1636@gmail.com>
2021-11-11 23:33:00 +0300

    cart project is read now. usereduce is the main goal of the project.

---
+++ 14-cart/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+
+// components
+import Navbar from './Navbar'
+import CartContainer from './CartContainer'
+// items
+
+function App() {
+  const {loading} = useGlobalContext()
+  if (loading) {
+    if(loading) {
+      return (
+        <div className='loading'>
+          <h1>Loading...</h1>
+        </div>
+
+
+
+      )
+    }
+  }
+  // if (loading) {
+  //   return (
+  //     <div className='loading'>
+  //       <h1>Loading...</h1>
+  //     </div>
+  //   )
+  // }
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  )
+}
+
+export default App
